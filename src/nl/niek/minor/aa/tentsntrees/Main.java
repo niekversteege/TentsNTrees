9f8801520ba1c77c6da09dab5444836b159324b2
Niek <niekversteege@gmail.com>
2012-10-09 22:06:21 +0200

    Init

---
+++ src/nl/niek/minor/aa/tentsntrees/Main.java
@@ -0,0 +1,15 @@
+package nl.niek.minor.aa.tentsntrees;
+
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
